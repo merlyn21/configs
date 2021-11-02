5387c6041e1819b6c8ab565e374a80dafc20a2c6
Nick <merlyn022@gmail.com>
2021-11-02 14:45:23 +0300

    Update begin_docker.sh

--- begin_docker.sh
+++ begin_docker.sh
@@ -3,7 +3,7 @@ apt-get install mc net-tools -y
 #Install docker
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
-apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
