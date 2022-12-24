f304035139ba7a3b241f1cf54382d27174099d7c
Nick <merlyn022@gmail.com>
2022-12-24 23:03:34 +0400

    Update install_agent.sh

--- zabbix/install_agent.sh
+++ zabbix/install_agent.sh
@@ -1,7 +1,7 @@
 wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-1+ubuntu20.04_all.deb
 dpkg -i zabbix-release_6.0-1+ubuntu20.04_all.deb
 apt update
-apt install zabbix-agent2
+apt install zabbix-agent2 acl
 
 
 openssl rand -hex 32 > zabbix_agent2.psk
