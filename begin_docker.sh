e408c1f9fea3516a514d5452990f43c339c8381b
Nick <merlyn022@gmail.com>
2023-05-25 22:09:24 +0300

    Update begin_docker.sh

--- begin_docker.sh
+++ begin_docker.sh
@@ -13,7 +13,7 @@ apt-get install docker-ce docker-ce-cli containerd.io -y
 
 
 #install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.18.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 #install girlab-runner
